a6dd5ae8fa251ba6504d7e83c9202ec6452a785b
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-25 20:30:29 +0330

    add routes to the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      This is csv project
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 }
 
