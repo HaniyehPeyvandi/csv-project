8ec1d3e41c38d3fe22e4e560cc82b76384fff2b1
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-26 09:15:06 +0330

    add form to get csv file in HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+
 const HomePage = () => {
-  return ( 
-    <div>This is home page</div>
-   );
-}
- 
+  const [file, setFile] = useState(null);
+
+  const changeHandler = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        component="form"
+        sx={{ bgcolor: "#ccc", pt: 2, pb: 2 }}
+      >
+        <Button color="secondary" component="label">
+          <input accept=".csv" type="file" onChange={changeHandler} />
+        </Button>
+        <Button type="submit" variant="contained" color="secondary">
+          show data
+        </Button>
+      </Box>
+    </Container>
+  );
+};
+
 export default HomePage;
