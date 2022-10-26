388de24af840d1e3ea7f88ce2a498ee1c85c2747
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-26 17:29:00 +0330

    add route for UsersPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import UsersPage from "./pages/UsersPage/UsersPage";
 
 function App() {
   return (
     <Routes>
+      <Route path="/users" element={<UsersPage />} />
       <Route path="/" element={<HomePage />} />
     </Routes>
   );
