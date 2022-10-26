82269794850c29d3161979b06ec75044ce1d779d
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-26 20:15:17 +0330

    add code for showing content of file in a table

--- src/pages/UsersPage/UsersPage.js
+++ src/pages/UsersPage/UsersPage.js
@@ -1,5 +1,43 @@
+import { useLocation } from "react-router-dom";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Container } from "@mui/system";
+
 const UsersPage = () => {
-  return <div>This is users page</div>;
+  const { state } = useLocation();
+  const { csvArray } = state;
+
+  return (
+    <Container component="main" maxWidth="md">
+      <TableContainer component={Paper} sx={{ mt: 3 }}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Index</TableCell>
+              <TableCell align="center">First Name</TableCell>
+              <TableCell align="center">Last Name</TableCell>
+              <TableCell align="center">User ID</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {csvArray.map((row) => (
+              <TableRow key={row.index}>
+                <TableCell align="center">{row.index}</TableCell>
+                <TableCell align="center">{row.firstName}</TableCell>
+                <TableCell align="center">{row.lastName}</TableCell>
+                <TableCell align="center">{row.id}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 };
 
 export default UsersPage;
