d8c15bc31438861463fa1a840cae452de4f2486a
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-27 10:06:12 +0330

    add code for handling empty csvArray

--- src/pages/UsersPage/UsersPage.js
+++ src/pages/UsersPage/UsersPage.js
@@ -6,7 +6,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Container } from "@mui/system";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
 const UsersPage = () => {
   const { state } = useLocation();
@@ -14,28 +15,36 @@ const UsersPage = () => {
 
   return (
     <Container component="main" maxWidth="md">
-      <TableContainer component={Paper} sx={{ mt: 3 }}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">Index</TableCell>
-              <TableCell align="center">First Name</TableCell>
-              <TableCell align="center">Last Name</TableCell>
-              <TableCell align="center">User ID</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {csvArray.map((row) => (
-              <TableRow key={row.index}>
-                <TableCell align="center">{row.index}</TableCell>
-                <TableCell align="center">{row.firstName}</TableCell>
-                <TableCell align="center">{row.lastName}</TableCell>
-                <TableCell align="center">{row.id}</TableCell>
+      {csvArray.length ? (
+        <TableContainer component={Paper} sx={{ mt: 3 }}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">Index</TableCell>
+                <TableCell align="center">First Name</TableCell>
+                <TableCell align="center">Last Name</TableCell>
+                <TableCell align="center">User ID</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {csvArray.map((row) => (
+                <TableRow key={row.index}>
+                  <TableCell align="center">{row.index}</TableCell>
+                  <TableCell align="center">{row.firstName}</TableCell>
+                  <TableCell align="center">{row.lastName}</TableCell>
+                  <TableCell align="center">{row.id}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <Paper sx={{ mt: 3, p: 2 }} align="center">
+          <Typography variant="h6" gutterBottom>
+            No users found.
+          </Typography>
+        </Paper>
+      )}
     </Container>
   );
 };
