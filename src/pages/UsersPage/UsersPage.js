5885e6bf6ccd74d80c1a7e61612318f04960cedf
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-10-28 10:24:29 +0330

    add table pagination in UsersPage

--- src/pages/UsersPage/UsersPage.js
+++ src/pages/UsersPage/UsersPage.js
@@ -6,6 +6,7 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TablePagination from "@mui/material/TablePagination";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
@@ -20,6 +21,21 @@ const UsersPage = () => {
 
   const [users, setUsers] = useState([]);
   const [filteredUsers, setFilteredUsers] = useState([]);
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, filteredUsers.length - page * rowsPerPage);
 
   useEffect(() => {
     setUsers(csvArray);
@@ -58,7 +74,7 @@ const UsersPage = () => {
         />
       </Box>
       {users.length ? (
-        <TableContainer component={Paper} sx={{ mt: 3 }}>
+        <TableContainer component={Paper} sx={{ mt: 3,mb:3 }}>
           <Table aria-label="simple table">
             <TableHead>
               <TableRow>
@@ -70,43 +86,64 @@ const UsersPage = () => {
             </TableHead>
             <TableBody>
               {filteredUsers.length ? (
-                filteredUsers.map((row) => (
-                  <TableRow key={row.index}>
-                    <TableCell align="center">{row.index}</TableCell>
-                    <TableCell align="center">
-                      {searchValue === "" ? (
-                        row.firstName
-                      ) : row.firstName
-                          .toLowerCase()
-                          .includes(searchValue.toLowerCase()) ? (
-                        <strong style={{color:"#7b1fa2"}}>{row.firstName}</strong>
-                      ) : (
-                        row.firstName
-                      )}
-                    </TableCell>
-                    <TableCell align="center">
-                      {searchValue === "" ? (
-                        row.lastName
-                      ) : row.lastName
-                          .toLowerCase()
-                          .includes(searchValue.toLowerCase()) ? (
-                        <strong style={{color:"#7b1fa2"}}>{row.lastName}</strong>
-                      ) : (
-                        row.lastName
-                      )}
-                    </TableCell>
-                    <TableCell align="center">{row.id}</TableCell>
-                  </TableRow>
-                ))
+                filteredUsers
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => (
+                    <TableRow key={row.index}>
+                      <TableCell align="center">{row.index}</TableCell>
+                      <TableCell align="center">
+                        {searchValue === "" ? (
+                          row.firstName
+                        ) : row.firstName
+                            .toLowerCase()
+                            .includes(searchValue.toLowerCase()) ? (
+                          <strong style={{ color: "#7b1fa2" }}>
+                            {row.firstName}
+                          </strong>
+                        ) : (
+                          row.firstName
+                        )}
+                      </TableCell>
+                      <TableCell align="center">
+                        {searchValue === "" ? (
+                          row.lastName
+                        ) : row.lastName
+                            .toLowerCase()
+                            .includes(searchValue.toLowerCase()) ? (
+                          <strong style={{ color: "#7b1fa2" }}>
+                            {row.lastName}
+                          </strong>
+                        ) : (
+                          row.lastName
+                        )}
+                      </TableCell>
+                      <TableCell align="center">{row.id}</TableCell>
+                    </TableRow>
+                  ))
               ) : (
                 <TableRow>
-                  <TableCell align="center" colSpan="4">
+                  <TableCell align="center" colSpan={4}>
                     No user found in search
                   </TableCell>
                 </TableRow>
               )}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={4} />
+                </TableRow>
+              )}
             </TableBody>
           </Table>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25,50,100]}
+            component="div"
+            count={filteredUsers.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            sx={{ display: 'flex',justifyContent: 'center',alignItems: 'center' }}
+          />
         </TableContainer>
       ) : (
         <Paper sx={{ mt: 3, p: 2 }} align="center">
